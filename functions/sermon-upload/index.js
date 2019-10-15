3e295d13f803e6bf4878b7a842d9bfa69abf75e2
Jonathan Haines <jonno.haines@gmail.com>
2019-10-15 22:16:07 +1100

    parse name and bucket from body

--- functions/sermon-upload/index.js
+++ functions/sermon-upload/index.js
@@ -1,8 +1,8 @@
 import createPresignedPost from '../../lib/create-presigned-post';
 import sendPresignedPost from './send-presigned-post';
 
-export default async function(data, context, callback) {
-	const {name, bucket} = data;
+export default async function(event, context, callback) {
+	const {name, bucket} = JSON.parse(event.body);
 	const [error, response] = await sendPresignedPost(createPresignedPost, {
 		bucket,
 		name
