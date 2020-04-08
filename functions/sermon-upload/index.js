32fc6955d60a42304f1fc9a86f604772ca1e475e
readeral <alan@readeral.com>
2020-04-08 17:24:14 +1000

    adding all region options to sermon-upload

--- functions/sermon-upload/index.js
+++ functions/sermon-upload/index.js
@@ -1,9 +1,23 @@
-import createPresignedPost from '../../lib/create-presigned-post';
+import {
+	southEast2PresignedPost,
+	usWest1PresignedPost,
+	usWest2PresignedPost,
+	usEast1PresignedPost,
+	usEast2PresignedPost
+} from '../../lib/create-presigned-post';
 import sendPresignedPost from './send-presigned-post';
 
+const regionFunctions = {
+	apSoutheast2: southEast2PresignedPost,
+	usWest1: usWest1PresignedPost,
+	usWest2: usWest2PresignedPost,
+	usEast1: usEast1PresignedPost,
+	usEast2: usEast2PresignedPost
+};
+
 export default async function(event, context, callback) {
-	const {name, bucket} = JSON.parse(event.body);
-	const [error, response] = await sendPresignedPost(createPresignedPost, {
+	const {name, bucket, region = 'apSoutheast2'} = JSON.parse(event.body);
+	const [error, response] = await sendPresignedPost(regionFunctions[region], {
 		bucket,
 		name
 	});
